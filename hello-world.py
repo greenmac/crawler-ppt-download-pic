ff4dbcb76cbfc6263c7955f3db091bea4f1ebb8b
greenmac <alwaysmac@msn.com>
2018-06-03 20:35:26 +0800

    2018/06/03 0,1,2,3,4,5

---
+++ hello-world.py
@@ -0,0 +1,22 @@
+# print("Hello World")
+
+# x=5
+# y=0.5
+# z="Hello World"
+# u="Hello World"
+# t=[5,0.5]
+
+# print(x)
+# print(y)
+# print(z)
+# print(u)
+# print(t[0])
+# print(t[1])
+# print("Hello World"+str(3))
+
+# print([1,2],[2,3])
+# print(5!=3)
+# print(len("Hello World!"))
+z=[3,1.5,"Hello",[2,3]]
+print(len(z))
+print(z[3])
