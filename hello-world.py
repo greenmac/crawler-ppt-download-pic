51eeaa886dab2a15361b7104997b4594a23a5f5f
greenmac <alwaysmac@msn.com>
2018-06-04 22:41:12 +0800

    2018/06/04 6,7,8,9

--- hello-world.py
+++ hello-world.py
@@ -17,6 +17,38 @@
 # print([1,2],[2,3])
 # print(5!=3)
 # print(len("Hello World!"))
-z=[3,1.5,"Hello",[2,3]]
-print(len(z))
-print(z[3])
+# z=[3,1.5,"Hello",[2,3]]
+# print(len(z))
+# print(z[3])
+
+# x=10
+# y=2
+# z=x+y
+# print(z>20 or z<15)
+# print(z>10 and z<15)
+# print(not z>10)
+
+# input("Input a number:")
+# x=input()
+# print(x)
+# print(type(x))
+
+# score=91
+# if score < 60:
+#     print("Fail")
+# elif score >90:
+#     print("Great")
+# else:
+#     print("Pass")
+
+# y=0
+# while y<10 :
+#     y=y+1
+# print(y)
+
+x=1
+base=1
+while x<1000:
+    x=x*base
+    base=base+1
+print(base)
