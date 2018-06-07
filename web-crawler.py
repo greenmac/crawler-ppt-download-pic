294cbdd8b447bf2a963cc002cb70973c1d7a4d3e
greenmac <alwaysmac@msn.com>
2018-06-07 22:45:48 +0800

    2018/06/07 18,19

--- web-crawler.py
+++ web-crawler.py
@@ -21,22 +21,36 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from urllib.request import urlretrieve
+import os
 
-url="https://www.ptt.cc/bbs/ToS/index3852.html"
-#要找出錯誤
-reg_imgur_file=re.compile('http[s]?://i.imgur.com/\w+\.(?:jpg|png|gif)')
-
-for round in range(3):
-    res=requests.get(url)
-    soup=BeautifulSoup(res.text,"html.parser")
-    articles=soup.select("div.title a")
-    pagin=soup.select('div.btn-group-paging a')
-    # page2_url=pagin[1]['href']
-    next_url="https://www.ptt.cc"+pagin[1]['href']
-    url=next_url
-
+def download_images(articles):
     for article in articles:
         print(article.text,article['href'])
+        if not os.path.isdir(os.path.join('download',article.text)):
+            os.mkdir(os.path.join('download',article.text))
         res=requests.get("https://www.ptt.cc"+article['href'])
-        images=reg_imgur_file(res.text)
+        images=reg_imgur_file.findall(res.text)
         print(images)
+        for image in set(images):
+            ID=re.search('http[s]?://[i.]*imgur.com/(\w+\.(?:jpg|png|gif))',image).group(1)
+            print(ID)
+            urlretrieve(image,os.path.join('download',article.text,ID))
+
+def crawler():
+    if not os.path.isdir('download'):
+        os.mkdir('download')
+    url="https://www.ptt.cc/bbs/ToS/index3852.html"
+    for round in range(3):
+        res=requests.get(url)
+        soup=BeautifulSoup(res.text,"html.parser")
+        articles=soup.select("div.title a")
+        pagin=soup.select('div.btn-group-paging a')
+        # page2_url=pagin[1]['href']
+        next_url="https://www.ptt.cc"+pagin[1]['href']
+        url=next_url
+
+        download_images(articles)
+
+reg_imgur_file=re.compile('http[s]?://[i.]*imgur.com/\w+\.(?:jpg|png|gif)')
+crawler()
